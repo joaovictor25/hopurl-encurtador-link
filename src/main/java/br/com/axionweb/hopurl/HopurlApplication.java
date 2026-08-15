667042a9ef1f6533df4fee8a600f6767d50f5596
joaovictor25 <batistacarneiro1997@gmail.com>
2026-08-15 20:19:18 -0300

    Commit inicial

---
+++ src/main/java/br/com/axionweb/hopurl/HopurlApplication.java
@@ -0,0 +1,13 @@
+package br.com.axionweb.hopurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/axionweb/hopurl/HopurlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axionweb.hopurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
